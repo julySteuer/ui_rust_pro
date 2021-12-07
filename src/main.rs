ccd0460aab9dd90e61a6120ca7ae6244906b9a38
ToastBreadMan <xghostx2709@gmail.com>
2021-12-07 22:03:27 +0100

    tryed enum concat

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
 use serde;
 use serde_derive::{Deserialize, Serialize};
 use serde_xml_rs::{from_str, to_string};
+use ui::hello;
 
-trait subItemtrait {
+macro_rules! cast_enum {
+    ($it:path, $ret_type:expr) => {
+        if let $it(e) = $ret_type {
+            Some(e)
+        }
+        else{
+            None
+        }
+    };
+}
 
+macro_rules! concat {
+    ($new_struct:ident,$new_enum:ident, $($elem:tt)*) => {
+        #[derive(Debug)]
+        enum $new_enum {
+            $($elem)*
+        }
+        struct $new_struct {
+            vals:$new_enum,
+            names: String
+        }
+    };
+}
+
+macro_rules! make {
+    ($new_struct:ident,$new_enum:ident, $($elem:expr)*) => {
+        #[derive(Debug)]
+        enum $new_enum {
+            $($elem)*
+        }
+        struct $new_struct {
+            vals:$new_enum,
+            names: String
+        }
+    };
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+enum subItemEnum {
+    subItem(subItem),
+    subItemTwo(subItemTwo)
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
@@ -11,26 +51,24 @@ struct subItem {
     test:String
 }
 
-impl subItemtrait for subItem {
-
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct subItemTwo {
+    test:String
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Item {
-    name: String,
-    source: String,
-    subItem: subItemtrait
-}
-
-
-#[derive(Debug, Deserialize)]
-pub struct RowValue {
-    pub value: i32,
+    #[serde(rename = "$value")]
+    events: Vec<subItemEnum>
 }
 
 fn main() {
-    let src = r#"<Item><subItem><test>Hello</test></subItem><name>Banana</name><source>Store</source></Item>"#;
-
+    let src = r#"<Item><subItem><test>Hello</test></subItem></Item>"#;
     let item: Item = from_str(src).unwrap();
-    println!("{}", item.subItem.test);
+    concat!(test, test1,Foo(subItem));
+    make!(world, ):
+    let new_sub = subItem{test:String::from("test")};
+    println!("{:?}", test1::Foo(new_sub));
+    cast_enum!(subItemEnum::subItem, &item.events[0]);
+    println!("{:?}", cast_enum!(subItemEnum::subItem, &item.events[0]).unwrap());
 }
