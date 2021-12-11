057d23adde1718248a5860193645bc33b0e1a6c6
ToastBreadMan <xghostx2709@gmail.com>
2021-12-12 00:41:56 +0100

    problem with macro

--- src/main.rs
+++ src/main.rs
@@ -2,54 +2,68 @@ use serde;
 use serde_derive::{Deserialize, Serialize};
 use serde_xml_rs::{from_str, to_string};
 use ui::hello;
+use std::fs;
+extern crate syn;
+extern crate proc_macro2;
+#[macro_use]
+extern crate quote;
 
+use syn::Ident;
+use proc_macro2::Span;
 
-//macro_rules! cast_enum {
-//    ($it:path, $ret_type:expr) => {
-//        if let $it(e) = $ret_type {
-//            Some(e)
-//        }
-//        else{
-//            None
-//        }
-//    };
-//}
+extern crate minidom;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-enum Base {
-    subItem(subItem),
-    subItemTwo(subItemTwo)
+use minidom::Element;
+
+macro_rules! cast_enum {
+    ($it:path, $ret_type:expr) => {
+        if let $it(e) = $ret_type {
+            Some(e)
+        }
+        else{
+            None
+        }
+    };
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-enum Extend {
-    subItemThree(subItemThree),
-    Base(Base)
+struct test {
+    pub x: isize
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct subItem {
-    test:String
+macro_rules! parse_xml_struct {
+    ($struct_name:ident) => {
+        let elem = &$struct_name {
+            x:1
+        };
+        println!("{}", elem.x);
+    };
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct subItemTwo {
-    test:String
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct subItemThree{
-    test:String
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Item {
-    #[serde(rename = "$value")]
-    events: Vec<Extend>,
+fn list_children(elem: minidom::node::Node){
+    if let minidom::node::Node::Element(val) = elem {
+        for iterator in val.children() {
+            let text = &iterator.text();
+            let text = text.trim();
+            let splitted = text.split("\r\n");
+            let mut end_strings: Vec<String> = Vec::new();
+            for split in splitted {
+                end_strings.push(String::from(split.trim()));
+            }
+            for split in end_strings {
+                println!("{}", split);
+            }
+            list_children(minidom::Node::Element(iterator.clone())); // has to include Node
+        }
+    }
 }
 
 fn main() {
-    let src = r#"<Item><subItem><test>Hello</test></subItem></Item>"#;
-    let item: Item = from_str(src).unwrap();
-    //println!("{:?}", cast_enum!(Base::subItem, &item.events[0]).unwrap());
+    let data:String = fs::read_to_string("assets/index.xml").unwrap();
+    let root: Element = data.parse().unwrap();
+    let def_ident = Ident::new("test", Span::call_site());
+    let exe = quote! {
+        parse_xml_struct!(#def_ident);
+    };
+    let t = proc_macro2::TokenStream::from(exe);
+    list_children(minidom::node::Node::Element(root));
 }
